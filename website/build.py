346df8896632c0ecfa8aa4ce8b87f575e7ca5a22
hzchenxiaobin <hzchenxiaobin@gmail.com>
2026-07-17 10:16:00 +0800

    feat(website): 接入 Prism C++ 语法高亮（prism-cpp.min.js）

--- website/build.py
+++ website/build.py
@@ -163,6 +163,7 @@ def page_template(title: str, nav_html: str, markdown: str) -> str:
     <link href="../css/prism-tomorrow.min.css" rel="stylesheet">
     <script src="../js/prism.min.js"></script>
     <script src="../js/prism-c.min.js"></script>
+    <script src="../js/prism-cpp.min.js"></script>
     <script>Prism.languages.cuda=Prism.languages.extend("c",{{builtin:/\\b(?:__global__|__device__|__host__|__shared__|__constant__|__managed__|__restrict__|__syncthreads|__threadfence|__threadfence_block|blockIdx|threadIdx|blockDim|gridDim|warpSize)\\b/}});</script>
     <script src="../js/prism-bash.min.js"></script>
     <script src="../js/prism-python.min.js"></script>
